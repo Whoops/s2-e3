79ba1327106f180ed453c230de6334a3424036d5
Walton Hoops <me@waltonhoops.com>
2010-10-16 15:25:36 -0600

    Add a help menu downcase all commands before processing print door list on each turn

--- lib/adventure.rb
+++ lib/adventure.rb
@@ -4,13 +4,28 @@ module Adventure
   extend self
   def run(config)
     World.load(config)
+    puts "At any time type help for a list of commands"
     while (true)
       do_turn
     end
   end
   
-  def do_turn()
+  def print_commands
+    puts "Command list:"
+    puts "\t help -Show this screen"
+    puts "\t go <direction>  -Goes in the direction specified"
+    puts "\t exit  -Exit the program"
+  end
+  
+  def print_state
     puts World.room.description
+    World.room.doors.keys.each do |direction|
+      puts "You see a door to the #{direction}"
+    end
+  end
+  
+  def do_turn()
+    print_state
     command = get_command
     puts case command[0]
     when 'go' then
@@ -20,13 +35,14 @@ module Adventure
         "You can't go that way"
       end
     when 'exit' then exit
+    when 'help' then print_commands
     else 'I have no idea what your talking about'
     end
   end
   
   def get_command()
     print "What will you do? > "
-    STDIN.gets.chomp.split(/\s/)
+    STDIN.gets.chomp.downcase.split(/\s/)
   end
   
 end
